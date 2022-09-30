954da9b3606cbfa916c6abc6cb84d4f9409354ca
Darshana Perera <darshana.saluka.10@gmail.com>
2022-09-30 13:15:42 +0530

    navbar created

---
+++ Components/Profile.js
@@ -0,0 +1,8 @@
+import Image from "next/image";
+export default function Profile() {
+  return (
+    <div>
+      <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+    </div>
+  );
+}
---
+++ Layouts/NavgBar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Profile from "../Components/Profile";
+
+export default function NavgBar() {
+  return (
+    <div>
+      <Profile />
+    </div>
+  );
+}
