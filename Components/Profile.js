8144bf6f268dc9eb31c56a4ff73c885f4939e1a1
Darshana Perera <darshana.saluka.10@gmail.com>
2022-09-30 13:35:53 +0530

    hardcoded data added for profile

--- Components/Profile.js
+++ Components/Profile.js
@@ -1,8 +1,24 @@
+import React from "react";
 import Image from "next/image";
 export default function Profile() {
+  const [userData, setUserData] = React.useState({
+    name: "",
+    year: "",
+    grade: "",
+    imgLink: "",
+  });
+  React.useEffect(() => {
+    setUserData({
+      name: "Darshana Perera",
+      year: "13",
+      grade: "C",
+      imgLink:
+        "https://www.pngmart.com/files/22/User-Avatar-Profile-PNG-Isolated-Transparent-Picture.png",
+    });
+  }, []);
   return (
     <div>
-      <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+      {/* <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} /> */}
     </div>
   );
 }
